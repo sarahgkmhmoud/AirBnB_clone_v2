ba4b10775821e36da01f4b32e10221976182058a
Sarahgad <sarahgad91@gmail.com>
2023-11-21 14:09:57 +0200

    sql db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,13 +1,48 @@
 #!/usr/bin/python3
 """data base storage"""
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
+# Get values from environment variables
+mysql_user = getenv("HBNB_MYSQL_USER")
+mysql_pwd = getenv("HBNB_MYSQL_PWD")
+mysql_host = getenv("HBNB_MYSQL_HOST")
+mysql_db = getenv("HBNB_MYSQL_DB")
+
+# Construct the database URL
+db_url = f"mysql+mysqldb://{mysql_user}:{mysql_pwd}@{mysql_host}/{mysql_db}"
+
+# Create the SQLAlchemy engine
 class DBStorage:
     __engine = None
     __session = None
     def __init__(self):
-        self.__engine = create_engine()
-        
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
+        metadata = MetaData(bind=self.__engine)
+        if getenv("HBNB_ENV") == 'test':
+            metadata.drop.all()
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+    def all(self, cls=None):
+        cls_dict = {}
+        if cls:
+            cls_list = self.__session.query(cls)
+        else:
+            cls_list = self.__session.query()
+        for instance in cls_list:
+            key = f"{instance.__classs__.__name__}.{instance.id}"
+            cls_dict [key] = instance 
+        return cls_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    
+
+    
--- models/state.py
+++ models/state.py
@@ -10,17 +10,17 @@ from models import storage
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
 
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="states", cascade='all, delete-orphan')
+    if os.getenv("HBNB_TYPE_STORAGE" == "db"):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", back_populates="states", cascade='all, delete-orphan')
 
-    if os.getenv("HBNB_TYPE_STORAGE" != "db"):
+    elif os.getenv("HBNB_TYPE_STORAGE" != "db"):
         @property
         def cities(self):
             cities_list = []
-            for city in storage.all(City).values()
+            for city in storage.all(City).values():
                 if City.state_id == self.id:
-                    cities_list.append(City.state_id)
+                    cities_list.append(City)
             return cities_list
-
