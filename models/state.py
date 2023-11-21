1abce964d69774a92bc56d56428a531cf8fb40d2
Sarahgad <sarahgad91@gmail.com>
2023-11-21 18:48:58 +0200

    edit delete

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+import models
 
 
 class State(BaseModel, Base):
@@ -12,13 +13,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", back_populates="states", cascade='all, delete-orphan')
+        cities = relationship("City", backref="state", cascade='all, delete-orphan')
 
     elif getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             cities_list = []
-            for city in storage.all(City).values():
+            for city in list(models.storage.all(City).values()):
                 if City.state_id == self.id:
                     cities_list.append(City)
             return cities_list
