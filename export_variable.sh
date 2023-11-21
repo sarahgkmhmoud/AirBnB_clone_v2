6ff313f305729a998283c15d27fde461df2b60da
Sarahgad <sarahgad91@gmail.com>
2023-11-21 16:18:14 +0200

    last edit

---
+++ export_variable.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Environment variables will be your best friend for this project!
+export HBNB_ENV=dev
+export HBNB_MYSQL_USER=user
+export HBNB_MYSQL_PWD=passwd
+export HBNB_MYSQL_HOST=localhost
+export HBNB_MYSQL_DB=database
+export HBNB_TYPE_STORAGE=db
+echo $HBNB_ENV
+echo $HBNB_MYSQL_USER
+echo $HBNB_MYSQL_PWD
+echo $HBNB_MYSQL_HOST
+echo $HBNB_MYSQL_DB
+echo $HBNB_TYPE_STORAGE
