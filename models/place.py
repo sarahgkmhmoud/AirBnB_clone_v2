8649a23c0464680816dfe356897722118aad99ed
ManarElsaid <elsaidmanar@gmail.com>
2023-11-21 14:10:16 -0800

    8

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
@@ -12,16 +12,16 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = column(String(60), nullable=False, ForeignKey("cities.id"))
-        user_id = column(String(60), nullable=False, ForeignKey("users.id"))
-        name = column(String(128), nullable=False)
-        description = column(String(1024), nullable=False)
-        number_rooms = column(Integer, nullable=False, default=0)
-        number_bathrooms = column(Integer, nullable=False, default=0)
-        max_guest = column(Integer, nullable=False, default=0)
-        price_by_night = column(Integer, nullable=False, default=0)
-        latitude = column(Float, nullable=False)
-        longitude = column(Float, nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
         reviews = relationship ('Review', backref="place", cascade='all, delete-orphan')
 
     elif getenv("HBNB_TYPE_STORAGE") != "db": 
