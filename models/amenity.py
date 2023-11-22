23b1be4d693e3ca7e79ec3c42b95b02af2c2ed45
Sarahgad <sarahgad91@gmail.com>
2023-11-22 02:29:33 +0200

    Try

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.place import Place, 
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 from sqlalchemy.orm import relationship
