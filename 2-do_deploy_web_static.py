7a8cd945809be9d3aef5464ea787c3b5fd9c08de
Sarahgad <sarahgad91@gmail.com>
2023-12-08 01:00:46 +0200

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """Write a Fabric script that generates a .tgz archive"""
-from fabric import Connection
 from fabric.api import run, env, put
+from os.path import exists
 env.hosts = ['54.86.220.157', '100.26.212.86']
  
 
 
 def do_deploy(archive_path):
-    if archive_path:
-        servers = ['ubuntu@54.86.220.157', 'ubuntu@100.26.212.86']
-        connection = Connection(servers).put(archive_path,'/tmp/' )
-        with Connection(servers) as result:
-            result.run('tar -xzvf /tmp/{archive_path} -C /data/web_static/releases/'
+    if exists(archive_path) is False:
+        return False
+    try:
+
+        run('tar -xzvf /tmp/{archive_path} -C /data/web_static/releases/'
                            .format(archive_path))
-            result.run('rm -r /tmp/')
-            result.run ('rm -r /data/web_static/current')
-            result.run ('sudo ln -sf /data/web_static/current /data/web_static/releases/')
+        run('rm -r /tmp/')
+        run ('rm -r /data/web_static/current')
+        run ('sudo ln -sf /data/web_static/current /data/web_static/releases/')
         return True
-    else:
+    except:
         return False
