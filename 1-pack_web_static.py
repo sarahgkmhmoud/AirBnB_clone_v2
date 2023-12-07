4a13925372d970846eafb46c6c7e458305b62b8e
Sarahgad <sarahgad91@gmail.com>
2023-12-07 23:39:06 +0200

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a Fabric script that generates a .tgz archive"""
+from fabric import local
+print(":")
+
+# def do_pack():
+#     locals("tar -czvf versions.tgz web_static/")
+
