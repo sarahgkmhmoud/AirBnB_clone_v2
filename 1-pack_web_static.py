19a85c6d3e2995ec627115376a8d123ced6f077e
Sarahgad <sarahgad91@gmail.com>
2023-12-08 00:17:05 +0200

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,39 @@
 #!/usr/bin/python3
 """Write a Fabric script that generates a .tgz archive"""
-from fabric import local
-print(":")
+from fabric.api import local
+from datetime import datetime
+from os.path import isdir
+
+
+def do_pack():
+    """Trying to do that"""
+    try:
+        data = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(data)
+        local(f"tar -cvzf {file_name} web_static")
+        return file_name
+    except:
+        return None
+# """
+# Fabric script that generates a tgz archive from the contents of the web_static
+# folder of the AirBnB Clone repo
+# """
+
+# from datetime import datetime
+# from fabric.api import local
+# from os.path import isdir
+
 
 # def do_pack():
-#     locals("tar -czvf versions.tgz web_static/")
-
+#     """generates a tgz archive"""
+#     try:
+#         date = datetime.now().strftime("%Y%m%d%H%M%S")
+#         if isdir("versions") is False:
+#             local("mkdir versions")
+#         file_name = "versions/web_static_{}.tgz".format(date)
+#         local("tar -cvzf {} web_static".format(file_name))
+#         return file_name
+#     except:
+#         return None
