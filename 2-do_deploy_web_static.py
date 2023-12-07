e1d0b88fb27d4b31cd962874f94c41272f2fe82c
Sarahgad <sarahgad91@gmail.com>
2023-12-08 01:16:59 +0200

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,12 +10,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-
-        run('tar -xzvf /tmp/{archive_path} -C /data/web_static/releases/'
-                           .format(archive_path))
-        run('rm -r /tmp/')
-        run ('rm -r /data/web_static/current')
-        run ('sudo ln -sf /data/web_static/current /data/web_static/releases/')
+        _file = archive_path.split("/")[-1]
+        match_path = _file.split('.')[0]
+        remote_path = '/data/web_static/releases/'
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {remote_path}{match_path}/')
+        run(f'tar -xzf /tmp/{_file} -C {remote_path}{match_path}')
+        run(f'rm -r /tmp/{_file}')
+        run(f'mv {remote_path}{match_path}/web_static/* {remote_path}{match_path}/')
+        run(f'rm -rf {remote_path}{match_path}/web_static/*')
+        run (f'rm -rf /data/web_static/current')
+        run (f'sudo ln -sf {remote_path}{match_path}/ /data/web_static/current')
         return True
     except:
         return False
