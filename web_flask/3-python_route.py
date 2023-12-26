9652b94f94e5ac0d0be28124affd22bf9849471d
Sarahgad <sarahgad91@gmail.com>
2023-12-26 21:00:41 +0200

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""0. Hello Flask!"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>",  strict_slashes=False)
+def show_C(text):
+    return f"C {text.replace('_',' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def show_python(text='is_cool'):
+    return f"Python {text.replace('_',' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
