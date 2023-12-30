82a22713ff038bae9d8caa3d4e9915cf3c8b7547
Sarahgad <sarahgad91@gmail.com>
2023-12-30 14:03:19 +0200

    8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0. Hello Flask!"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
