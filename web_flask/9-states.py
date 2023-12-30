9d4ac6bd314d7a73f63feb5badf4a6797569ca6d
Sarahgad <sarahgad91@gmail.com>
2023-12-30 22:06:57 +0200

    edit

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-def states_list():
+def states_list_id():
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
